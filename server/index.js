1d5cace268bf35e519d4b8064833e8ea0bf2ea04
Minh Quang <quangminhit94@gmail.com>
2019-11-01 15:00:24 +0700

    Init chat

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+// const socketio = require('socket.io')
+// const http = require('http')
+
+const app = express()
+// const port = process.env.PORT || 3000
+
+// const server = http.createServer(app)
+// const io = socketio(server)
+
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(port, () => console.log(`Example app listening on port port!`))
